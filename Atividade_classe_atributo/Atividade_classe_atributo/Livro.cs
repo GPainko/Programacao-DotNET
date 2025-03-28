563a5c803c8a579a57ce6884f0fa23cd387abe34
Guilherme Painko Scalcon <guilherme.Painko@outlook.com>
2025-03-28 11:31:11 -0300

    Atualização aulas

--- Atividade_classe_atributo/Atividade_classe_atributo/Livro.cs
+++ Atividade_classe_atributo/Atividade_classe_atributo/Livro.cs
@@ -8,5 +8,11 @@ namespace Atividade_classe_atributo
 {
     internal class Livro
     {
+        //4 - Crie uma classe Livro com os atributos título, autor e ano de publicação.
+        //Faça a instanciação de 3 objetos e imprima as informações do livro na tela.
+
+        public String titulo;
+        public String autor;
+        public int anoPublicacao;
     }
 }

faa04ea57883a06052b32d537c858f6fbb372341
Guilherme Painko Scalcon <guilherme.Painko@outlook.com>
2025-04-04 03:06:55 -0300

    Atualizaçõa aula

--- exercico_metodo/exercico_metodo/Aluno.cs
+++ exercico_metodo/exercico_metodo/Aluno.cs
@@ -8,5 +8,30 @@ namespace exercico_metodo
 {
     internal class Aluno
     {
+
+        // 1- Escreva uma classe Aluno contendo todos os atributos de um aluno. Faça métodos para apresentar os dados.
+        // Faça a leitura pelo teclado dos atributos e crie um construtor para fazer o instanciamento.
+
+        public String nome;
+        public int matricula;
+        public int idade;
+        public int semestre;
+
+        public Aluno(string nome, int matricula, int idade, int semestre) {
+            this.nome = nome;
+            this.matricula = matricula;
+            this.idade = idade;
+            this.semestre = semestre;
+        }
+
+        public void dadosAluno()
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("nome:" + nome);
+            Console.WriteLine("Matricula:" + matricula);
+            Console.WriteLine("idade:" + idade);
+            Console.WriteLine("semestre:" + semestre);
+            Console.WriteLine("----------------------");
+        }
     }
 }
